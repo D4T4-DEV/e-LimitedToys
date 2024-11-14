4f6059224c87928fc2e92bdd6c2cd573a2c28f0c
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2024-11-13 22:08:34 -0600

    Acciones para consumir la API
    Se crea las acciones para consumir la API en el LOGIN, esto solo es un pequeño paso para lo que falta

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import './Auth.css';
+import { EncriptarDatos } from '../security/Encr_decp';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import qs from 'qs';
+
+const { BASE_URL_API_E_LIMITED } = import.meta.env;
 
 const loginSchema = z.object({
   email: z.string().email({ message: 'Correo electronico invalido' }),
@@ -10,6 +15,7 @@ const loginSchema = z.object({
 });
 
 type FormDataLogin = z.infer<typeof loginSchema>;
+
 
 const Login: React.FC = () => {
 
@@ -20,9 +26,10 @@ const Login: React.FC = () => {
   const handleLogin = async (data: FormDataLogin) => {
     // Lógica para el inicio de sesión
     console.log('Usuario inició sesión:', data);
+    await IniciarSesion(data);
   };
 
-  return (  
+  return (
     <div className="auth-container">
       <h2>Iniciar sesión</h2>
       <form onSubmit={handleSubmit(handleLogin)}>
@@ -33,7 +40,7 @@ const Login: React.FC = () => {
         />
         {errors.email && <span className="error-message">{errors.email.message}</span>}
 
-        <input  
+        <input
           type="password"
           placeholder="Contraseña"
           {...register("password")}
@@ -44,6 +51,42 @@ const Login: React.FC = () => {
       <h3>¿No tienes cuenta? Crea una aquí</h3>
     </div>
   );
+};
+
+// MEDIO PARA HACER LA PETICIÓN A LA API
+
+type JsonData = { [key: string]: any }; // -> Objeto JSON 
+export interface Respuesta {
+  status: number;
+  message: string;
+  data?: JsonData
+}
+
+const IniciarSesion = async (dataRecibida: any): Promise<void> => {
+
+  // Creamos los datos en formato application/x-www-form-urlencoded
+  const data = qs.stringify({
+    'datos_encriptados': await EncriptarDatos(dataRecibida),
+  });
+
+  // Configuracion de la peticion que generá AXIOS
+  const config: AxiosRequestConfig = {
+    method: 'POST',
+    maxBodyLength: Infinity,
+    url: `${BASE_URL_API_E_LIMITED}/usuarios/login/`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data,
+  };
+
+  try {
+    // tramite y espera de respuesta
+    const response: AxiosResponse<Respuesta> = await axios.request(config);
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.error(error); // -> Esto debe ser tratado mejor y no en consola del cliente OJO 
+  }
 };
 
 export default Login;
