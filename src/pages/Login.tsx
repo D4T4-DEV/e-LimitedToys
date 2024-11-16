438c264f5d4e46a72cf5f319dd292d446f094b4f
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2024-11-15 20:07:38 -0600

    Mejoras del codigo del login
    Se mejora la manera de hacer la petición en AXIOS y la interfaz usada

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
 import './Auth.css';
-import { EncriptarDatos } from '../security/Encriptar_data';
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import React from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { useForm } from 'react-hook-form';
 import qs from 'qs';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { EncriptarDatos } from '../security/Encriptar_data';
 import { RespuestaApi } from '../Interfaces/Response_API';
+import { loginSchema } from '../Interfaces/LoginInterface';
 
 const { VITE_BASE_URL_API } = import.meta.env;
 
-const loginSchema = z.object({
-  email: z.string().email({ message: 'Correo electronico invalido' }),
-  password: z.string()
-});
-
 type FormDataLogin = z.infer<typeof loginSchema>;
-
 
 const Login: React.FC = () => {
 
@@ -59,34 +54,29 @@ const Login: React.FC = () => {
 
 const IniciarSesion = async (dataRecibida: any): Promise<void> => {
 
-  if(!VITE_BASE_URL_API){
+  if (!VITE_BASE_URL_API) {
     throw new Error("No tengo una api a la cual contactarme")
   }
 
-  const datosEncriptados =  await EncriptarDatos(dataRecibida);
+  const datosEncriptados = await EncriptarDatos(dataRecibida);
 
   // Creamos los datos en formato application/x-www-form-urlencoded
   const data = qs.stringify({
     'datos_encriptados': datosEncriptados,
   });
-
-  // Configuracion de la peticion que generá AXIOS
-  const config: AxiosRequestConfig = {
-    method: 'POST',
-    maxBodyLength: Infinity,
-    url: `${VITE_BASE_URL_API}/usuarios/login/`,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data,
-  };
-
+  
   try {
     // tramite y espera de respuesta
-    const response: AxiosResponse<RespuestaApi> = await axios.request(config);
-    console.log(JSON.stringify(response.data));
+    const response: AxiosResponse<RespuestaApi> = await axios.post(`${VITE_BASE_URL_API}/usuarios/login/`,data);
+    // console.log((response.data));
+    console.log((response.data));
   } catch (error) {
-    console.error(error); // -> Esto debe ser tratado mejor y no en consola del cliente OJO 
+    if(axios.isAxiosError(error)){
+      console.error(error.message); // -> Esto debe ser tratado mejor y no en consola del cliente OJO 
+    }
+    else{
+      console.error(error); // -> Esto debe ser tratado mejor y no en consola del cliente OJO
+    }
   }
 };
 
