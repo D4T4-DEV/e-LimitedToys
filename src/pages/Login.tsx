1bf251622babbf67f1a8325b5d99f9606c657005
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2024-11-19 22:57:11 -0600

    Update Login.tsx
    Se usa react tostify para manejo de erorres y si este esta logueado correctamente se redirige a home, ya si no se utiliza la funcion del contexto para limpiar el error, esto previene que no se muestre mas de una vez.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import './Auth.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -8,10 +8,15 @@ import { AppDispatch, RootState } from '../redux/store';
 import { loginSchema } from '../Interfaces/LoginInterface';
 import { iniciarSesion } from '../redux/Trunks/userTrunk';
 import LoadingModal from '../components/LoadignModal';
+import { useNavigate } from 'react-router-dom';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { limpiarError } from '../redux/Slides/userSlice';
 
 type FormDataLogin = z.infer<typeof loginSchema>;
 
 const Login: React.FC = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const { status, error, currentUser } = useSelector(
     (state: RootState) => state.users
@@ -25,13 +30,43 @@ const Login: React.FC = () => {
     dispatch(iniciarSesion(data));
   };
 
+  useEffect(() => {
+    // Si esta logueado no lo deja acceder a esta pagina, 
+    // lo redirigira a la pagina principal
+    if (currentUser) {
+      navigate('/');
+    }
+  }, [currentUser, navigate]);
+
+  useEffect(() => {
+    // Mostrar notificación del error proveniente de la API o otro medio
+    if (status === "failed" && error) {
+      toast.error(error, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        closeButton: (
+          <button style={{ all: "unset", cursor: "pointer", color: 'grey'}}>
+            &#10006;
+          </button>
+        ),
+      });
+      dispatch(limpiarError());
+    }
+  }, [status, error, dispatch]);
+
   return (
     <div className="auth-container">
       <h2>Iniciar sesión</h2>
       {status === "loading" &&
         <LoadingModal isOpen={status === "loading"} />
       }
-      {status === "failed" && <p className="error-message">Error: {error}</p>}
       {currentUser && <p>Bienvenido, {currentUser.nick}</p>}
       <form onSubmit={handleSubmit(handleLogin)}>
         <input
@@ -41,7 +76,6 @@ const Login: React.FC = () => {
           {...register("email")}
         />
         {errors.email && <span className="error-message">{errors.email.message}</span>}
-
         <input
           type="password"
           placeholder="Contraseña"
@@ -54,6 +88,8 @@ const Login: React.FC = () => {
         </button>
       </form>
       <h3>¿No tienes cuenta? Crea una aquí</h3>
+      {/* Contenedor para las notificaciones por toastify este muestra los errores nada mas */}
+      <ToastContainer />
     </div>
   );
 };
