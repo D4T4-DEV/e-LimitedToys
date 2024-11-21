d81610757613900a3f98380d07eaba342d03addd
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2024-11-20 19:29:03 -0600

    Update App.tsx
    Se usa el ruteo utilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import ProductCatalog from './pages/ProductCatalog';
+import { AppRoutes } from './Router/routes';
+
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/catalog" element={<ProductCatalog />} />
-        </Routes>
+        <AppRoutes /> {/* Renderiza las rutas desde un componente existente en rutas*/}
       </Layout>
     </Router>
   );
